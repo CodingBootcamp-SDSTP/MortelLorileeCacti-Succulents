0a60e08fceaf74f3c07f957b67a5fed45fe5c73f
LorileeMortel <lorilee_victory@yahoo.com.sg>
2018-06-20 15:50:06 +0800

    Third Commit 6/20/2018

---
+++ Plant.java
@@ -0,0 +1,93 @@
+public class Plant
+{
+	private String pid;
+	private String name;
+	private String type;
+	private String origin;
+	private String category;
+	private String scientificname;
+	private String alsoknownas;
+	private String color;
+	private String howtogrow;
+	private String image;
+
+	public void setPid(String pids) {
+		pid = pids;
+	}
+
+	public String getPid() {
+		return(pid);
+	}
+
+	public void setName(String names) {
+		name = names;
+	}
+
+	public String getName() {
+		return(name);
+	}
+
+	public void setType(String types) {
+		type = types;
+	}
+
+	public String getType() {
+		return(type);
+	}
+
+	public void setOrigin(String origins) {
+		origin = origins;
+	}
+
+	public String getOrigin() {
+		return(origin);
+	}
+
+	public void setCategory(String categories) {
+		category = categories;
+	}
+
+	public String getCategory() {
+		return(category);
+	}
+
+	public void setScientificName(String scientificnames) {
+		scientificname = scientificnames;
+	}
+
+	public String getScientificName() {
+		return(scientificname);
+	}
+
+	public void setAlsoKnownAs(String alsoknownass) {
+		alsoknownas = alsoknownass;
+	}
+
+	public String getAlsoKnownAs() {
+		return(alsoknownas);
+	}
+
+	public void setColor(String colors) {
+		color = colors;
+	}
+
+	public String getColors() {
+		return(color);
+	}
+
+	public void setHowToGrow(String howtogrows) {
+		howtogrow = howtogrows;
+	}
+
+	public String getHowToGrow() {
+		return(howtogrow);
+	}
+
+	public void setImage(String images) {
+		image = images;
+	}
+
+	public String getImage() {
+		return(image);
+	}
+}
